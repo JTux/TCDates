2c37675fe4815a08e2c4f6039c08363bc08d3f4d
Joshua Tucker <the1jtux@gmail.com>
2021-07-06 10:41:41 -0400

    Basic XML writing

--- TCDates/Form1.Designer.cs
+++ TCDates/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace TCDates
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bigRedButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bigRedButton
+            // 
+            this.bigRedButton.BackColor = System.Drawing.Color.Red;
+            this.bigRedButton.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bigRedButton.ForeColor = System.Drawing.Color.White;
+            this.bigRedButton.Location = new System.Drawing.Point(295, 159);
+            this.bigRedButton.Name = "bigRedButton";
+            this.bigRedButton.Size = new System.Drawing.Size(215, 90);
+            this.bigRedButton.TabIndex = 0;
+            this.bigRedButton.Text = "Click Me";
+            this.bigRedButton.UseVisualStyleBackColor = false;
+            this.bigRedButton.Click += new System.EventHandler(this.bigRedButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bigRedButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bigRedButton;
     }
 }
 
--- TCDates/Form1.cs
+++ TCDates/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace TCDates
 {
@@ -17,5 +19,21 @@ namespace TCDates
             InitializeComponent();
         }
 
+        private void bigRedButton_Click(object sender, EventArgs e)
+        {
+            WriteToXML();
+        }
+
+        private void WriteToXML()
+        {
+            XmlTextWriter textWriter = new("test.xml", null);
+            textWriter.WriteStartDocument();
+            textWriter.WriteComment("First comment");
+            textWriter.WriteStartElement("Test");
+            textWriter.WriteStartElement("Test2");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+        }
     }
 }
